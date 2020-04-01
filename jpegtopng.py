58545a55996bfedbf6b83152abb551efed7db219
Conner <connerccook@csu.fullerton.edu>
2020-04-01 15:58:43 -0700

    initial commit

---
+++ jpegtopng.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from PIL import Image
+
+#grab first and second argument
+#check if the new file exists, if not then create a new one
+#loop thru the jpegs and convert to png
+#save all new png files to a folder
+def main():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
